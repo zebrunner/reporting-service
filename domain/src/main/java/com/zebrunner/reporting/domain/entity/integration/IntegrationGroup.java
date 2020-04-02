a779f29a15e9c2396c02a7ec7a3cceb225249fcb
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-04-02 21:20:03 +0300

    Groups fetching fixed

--- domain/src/main/java/com/zebrunner/reporting/domain/entity/integration/IntegrationGroup.java
+++ domain/src/main/java/com/zebrunner/reporting/domain/entity/integration/IntegrationGroup.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
