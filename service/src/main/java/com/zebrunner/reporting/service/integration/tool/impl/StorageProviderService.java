2e461a8b42a526c9910b19eca0681a9e9bb29b36
brutskov <rutkovba@gmail.com>
2020-04-15 10:13:10 +0300

    Bug connected with exception that throws if aws integration is disabled was fixed

--- service/src/main/java/com/zebrunner/reporting/service/integration/tool/impl/StorageProviderService.java
+++ service/src/main/java/com/zebrunner/reporting/service/integration/tool/impl/StorageProviderService.java
@@ -2,6 +2,7 @@ package com.zebrunner.reporting.service.integration.tool.impl;
 
 import com.zebrunner.reporting.domain.dto.aws.FileUploadType;
 import com.zebrunner.reporting.domain.dto.aws.SessionCredentials;
+import com.zebrunner.reporting.service.exception.IllegalOperationException;
 import com.zebrunner.reporting.service.integration.IntegrationService;
 import com.zebrunner.reporting.service.integration.tool.AbstractIntegrationService;
 import com.zebrunner.reporting.service.integration.tool.adapter.storageprovider.StorageProviderAdapter;
@@ -36,8 +37,12 @@ public class StorageProviderService extends AbstractIntegrationService<StoragePr
     }
 
     public Optional<SessionCredentials> getTemporarySessionCredentials() {
-        StorageProviderAdapter adapter = getDefaultAdapterByType();
-        return adapter.getTemporarySessionCredentials(storageProviderTokenExpiration);
+        try {
+            StorageProviderAdapter adapter = getDefaultAdapterByType();
+            return adapter.getTemporarySessionCredentials(storageProviderTokenExpiration);
+        } catch (IllegalOperationException e) {
+            return Optional.empty();
+        }
     }
 
 }
