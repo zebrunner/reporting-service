82a8d8f60256a5b34faa73455540aaaf58d9595f
tsvrko <tsvrko@gmail.com>
2020-04-21 18:00:09 +0300

    Refactored

--- service/src/main/java/com/zebrunner/reporting/service/integration/tool/adapter/notificationservice/SlackIntegrationAdapter.java
+++ service/src/main/java/com/zebrunner/reporting/service/integration/tool/adapter/notificationservice/SlackIntegrationAdapter.java
@@ -11,15 +11,14 @@ import in.ashwanthkumar.slack.webhook.service.SlackService;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 public class SlackIntegrationAdapter extends AbstractIntegrationAdapter implements NotificationServiceAdapter {
 
     private static final String ERR_MSG_SLACK_CONNECTION_IS_NOT_ESTABLISHED = "Slack connection is not established";
-    public static final String ERR_MSG_UNABLE_TO_PUSH_SLACK_NOTIFICATION = "Unable to push Slack notification in channel {} ";
 
     private static final String RESULTS_PATTERN = "Passed: %d, Failed: %d, Known Issues: %d, Skipped: %d";
     private final static String INFO_PATTERN = "%1$s\n<%2$s|Open in Zafira>";
@@ -59,7 +58,29 @@ public class SlackIntegrationAdapter extends AbstractIntegrationAdapter implemen
 
     @Override
     public boolean isConnected() {
-        return push(null, new SlackMessage(StringUtils.EMPTY), null);
+        boolean result;
+        try {
+            /*
+            Slack webHook doesn't provide any functionality to check it's connectivity,
+            so, to make sure it is valid and avoid test messages in channels,
+            we can push empty message with null channel name and attachment
+            */
+            push(null, new SlackMessage(StringUtils.EMPTY));
+            result = true;
+        } catch (IOException e) {
+            LOGGER.error(ERR_MSG_SLACK_CONNECTION_IS_NOT_ESTABLISHED + "\n" + e.getMessage());
+             /*
+             Based on slackService functionality method push is void and in case of 4** response
+             throws exception with message containing status code.
+             400 is expected status code in case when valid webhook receives null message
+             which we on purpose send checking connectivity
+             */
+            result = e.getMessage().contains("400");
+        } catch (Exception e) {
+            LOGGER.error(ERR_MSG_SLACK_CONNECTION_IS_NOT_ESTABLISHED, e);
+            result = false;
+        }
+        return result;
     }
 
     @Override
@@ -68,7 +89,11 @@ public class SlackIntegrationAdapter extends AbstractIntegrationAdapter implemen
         if (StringUtils.isNotEmpty(channels)) {
             SlackAttachment attachment = generateSlackAttachment(testRun, notificationProperties);
             Arrays.stream(channels.split(",")).forEach(channel -> {
-                push(channel, new SlackMessage(), attachment);
+                try {
+                    push(channel, attachment);
+                } catch (IOException e) {
+                    LOGGER.error("Unable to push Slack notification");
+                }
             });
         }
     }
@@ -92,11 +117,10 @@ public class SlackIntegrationAdapter extends AbstractIntegrationAdapter implemen
         String resultsMessage = String.format(RESULTS_PATTERN, testRun.getPassed(), testRun.getFailed(), testRun.getFailedAsKnown(), testRun.getSkipped());
 
         SlackAttachment slackAttachment = new SlackAttachment("");
-        slackAttachment
-                .preText(mainMessage)
-                .color(attachmentColor)
-                .addField(new SlackAttachment.Field("Test Results", resultsMessage, false))
-                .fallback(mainMessage + "\n" + resultsMessage);
+        slackAttachment.preText(mainMessage)
+                       .color(attachmentColor)
+                       .addField(new SlackAttachment.Field("Test Results", resultsMessage, false))
+                       .fallback(mainMessage + "\n" + resultsMessage);
 
         if (testRun.getComments() != null) {
             slackAttachment.addField(new SlackAttachment.Field("Comments", testRun.getComments(), false));
@@ -104,20 +128,12 @@ public class SlackIntegrationAdapter extends AbstractIntegrationAdapter implemen
         return slackAttachment;
     }
 
-    private boolean push(String channel, SlackMessage message, SlackAttachment slackAttachment) {
-        boolean isConnected = false;
-        List<SlackAttachment> attachments = slackAttachment != null ? List.of(slackAttachment) : Collections.emptyList();
-        try {
-            slackService.push(webhookUrl, message, author, image, channel, null, attachments);
-        } catch (IOException e) {
-            if (e.getMessage().contains("400")) {
-                isConnected = true;
-            }
-            LOGGER.error(ERR_MSG_UNABLE_TO_PUSH_SLACK_NOTIFICATION, channel);
-        } catch (Exception e) {
-            LOGGER.error(ERR_MSG_SLACK_CONNECTION_IS_NOT_ESTABLISHED + "\n" + e.getMessage());
-        }
-        return isConnected;
+    private void push(String channel, SlackAttachment slackAttachment) throws IOException {
+        slackService.push(webhookUrl, new SlackMessage(), author, image, channel, null, Collections.singletonList(slackAttachment));
+    }
+
+    private void push(String channel, SlackMessage message) throws IOException {
+        slackService.push(webhookUrl, message, author, image, channel, null, new ArrayList<>());
     }
 
 }
