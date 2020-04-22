df5b3f834a8a55f355a131d2684bc8dfb9d53471
tsvrko <tsvrko@gmail.com>
2020-04-22 10:43:47 +0300

    Integration extracted correctly

--- service/src/main/java/com/zebrunner/reporting/service/ConfigurationService.java
+++ service/src/main/java/com/zebrunner/reporting/service/ConfigurationService.java
@@ -4,9 +4,8 @@ import com.zebrunner.reporting.domain.db.TestRun;
 import com.zebrunner.reporting.domain.entity.integration.Integration;
 import com.zebrunner.reporting.domain.entity.integration.IntegrationType;
 import com.zebrunner.reporting.service.exception.ResourceNotFoundException;
+import com.zebrunner.reporting.service.integration.IntegrationService;
 import com.zebrunner.reporting.service.integration.IntegrationTypeService;
-import com.zebrunner.reporting.service.integration.core.IntegrationInitializer;
-import com.zebrunner.reporting.service.integration.tool.AbstractIntegrationService;
 import com.zebrunner.reporting.service.integration.tool.impl.AutomationServerService;
 import com.zebrunner.reporting.service.integration.tool.impl.NotificationService;
 import com.zebrunner.reporting.service.integration.tool.impl.TestCaseManagementService;
@@ -15,6 +14,8 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 import java.util.Map;
 
 import static com.zebrunner.reporting.service.exception.ResourceNotFoundException.ResourceNotFoundErrorDetail.TEST_RUN_NOT_FOUND;
@@ -28,7 +29,7 @@ public class ConfigurationService {
     private final URLResolver urlResolver;
     private final AutomationServerService automationServerService;
     private final IntegrationTypeService integrationTypeService;
-    private final IntegrationInitializer integrationInitializer;
+    private final IntegrationService integrationService;
     private final TestCaseManagementService testCaseManagementService;
     private final TestRunService testRunService;
     private final NotificationService notificationService;
@@ -40,14 +41,15 @@ public class ConfigurationService {
             URLResolver urlResolver,
             AutomationServerService automationServerService,
             IntegrationTypeService integrationTypeService,
-            IntegrationInitializer integrationInitializer, TestCaseManagementService testCaseManagementService,
+            IntegrationService integrationService,
+            TestCaseManagementService testCaseManagementService,
             TestRunService testRunService,
             NotificationService notificationService
     ) {
         this.urlResolver = urlResolver;
         this.automationServerService = automationServerService;
         this.integrationTypeService = integrationTypeService;
-        this.integrationInitializer = integrationInitializer;
+        this.integrationService = integrationService;
         this.testCaseManagementService = testCaseManagementService;
         this.testRunService = testRunService;
         this.notificationService = notificationService;
@@ -83,8 +85,9 @@ public class ConfigurationService {
 
     private boolean isSlackAvailable() {
         IntegrationType defaultType = integrationTypeService.retrieveByName(notificationService.getDefaultType());
+        List<Integration> integrations = integrationService.retrieveByIntegrationsTypeName(defaultType.getName());
         String integrationName = "SLACK";
-        Integration slackIntegration = defaultType.getIntegrations()
+        Integration slackIntegration = integrations
                                                   .stream()
                                                   .filter(integration -> integrationName.equals(integration.getName()))
                                                   .findFirst()
