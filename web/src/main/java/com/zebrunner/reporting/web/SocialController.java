e30481f6446af54a0e737ff0496849b892da5c02
brutskov <rutkovba@gmail.com>
2020-06-08 10:51:24 +0300

    Minor fixes were added

--- web/src/main/java/com/zebrunner/reporting/web/SocialController.java
+++ web/src/main/java/com/zebrunner/reporting/web/SocialController.java
@@ -1,27 +1,14 @@
 package com.zebrunner.reporting.web;
 
-import com.zebrunner.reporting.domain.db.Invitation;
-import com.zebrunner.reporting.domain.dto.auth.InvitationListType;
-import com.zebrunner.reporting.domain.dto.auth.InvitationType;
-import com.zebrunner.reporting.persistence.dao.mysql.application.search.SearchCriteria;
-import com.zebrunner.reporting.persistence.dao.mysql.application.search.SearchResult;
-import com.zebrunner.reporting.service.InvitationService;
-import com.zebrunner.reporting.service.TestService;
 import com.zebrunner.reporting.service.TwitterService;
-import com.zebrunner.reporting.web.documented.InvitationDocumentedController;
 import com.zebrunner.reporting.web.documented.SocialDocumentedController;
 import lombok.RequiredArgsConstructor;
-import org.dozer.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @CrossOrigin
