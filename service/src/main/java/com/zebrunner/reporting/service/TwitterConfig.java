27f11bb09874c05e59af6f8dd58f9bd65c6e07ac
Bogdan Rutskov <rutkovba@gmail.com>
2020-06-08 11:22:11 +0300

    Minor fixes were added (#86)

--- service/src/main/java/com/zebrunner/reporting/service/TwitterConfig.java
+++ service/src/main/java/com/zebrunner/reporting/service/TwitterConfig.java
@@ -1,20 +1,13 @@
 package com.zebrunner.reporting.service;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
-import javax.annotation.PostConstruct;
-import java.util.UUID;
-
 @Configuration
+@ConditionalOnProperty(name = "twitter.enabled", havingValue = "true")
 public class TwitterConfig {
 
     private final String consumerKey;
