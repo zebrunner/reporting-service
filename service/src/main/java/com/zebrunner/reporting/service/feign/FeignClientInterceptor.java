9b2465763759449ad55b672514bde5fb2d77ce39
tsvrko <tsvrko@gmail.com>
2020-06-15 16:29:29 +0300

    Token is stored in user authentication

--- service/src/main/java/com/zebrunner/reporting/service/feign/FeignClientInterceptor.java
+++ service/src/main/java/com/zebrunner/reporting/service/feign/FeignClientInterceptor.java
@@ -1,23 +1,17 @@
 package com.zebrunner.reporting.service.feign;
 
 import com.zebrunner.reporting.domain.dto.auth.AuthenticatedUser;
-import com.zebrunner.reporting.persistence.utils.TenancyContext;
-import com.zebrunner.reporting.service.JWTService;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class FeignClientInterceptor implements RequestInterceptor {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String TOKEN_TYPE = "Bearer";
-
-    private final JWTService jwtService;
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
@@ -25,9 +19,7 @@ public class FeignClientInterceptor implements RequestInterceptor {
 
         if (authentication != null && authentication.getPrincipal() instanceof AuthenticatedUser) {
             AuthenticatedUser authenticatedUser = (AuthenticatedUser) authentication.getPrincipal();
-            String token = jwtService.generateAuthenticationToken(authenticatedUser.getId(),
-                    authenticatedUser.getUsername(), TenancyContext.getTenantName(), authenticatedUser.getPermissions());
-            requestTemplate.header(AUTHORIZATION_HEADER, String.format("%s %s", TOKEN_TYPE, token));
+            requestTemplate.header(AUTHORIZATION_HEADER, String.format("%s %s", TOKEN_TYPE, authenticatedUser.getToken()));
         }
     }
 }
