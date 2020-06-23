82e571c6add280abcb30f3fc49cf56a8084a4012
Aliksei Khursevich <hursevich@gmail.com>
2020-06-23 16:34:41 +0300

    - added validation for supported twitter accounts

--- web/src/main/java/com/zebrunner/reporting/web/SocialController.java
+++ web/src/main/java/com/zebrunner/reporting/web/SocialController.java
@@ -1,6 +1,7 @@
 package com.zebrunner.reporting.web;
 
 import com.zebrunner.reporting.service.TwitterService;
+import com.zebrunner.reporting.service.exception.ForbiddenOperationException;
 import com.zebrunner.reporting.web.documented.SocialDocumentedController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -8,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -18,11 +20,17 @@ import java.util.List;
 @ConditionalOnProperty(name = "twitter.enabled", havingValue = "true")
 public class SocialController extends AbstractController implements SocialDocumentedController {
 
+    private static final String ZEBRUNNER_NEWS = "zebrunner_news";
+
     private final TwitterService twitterService;
 
     @GetMapping("/twitter/timeline")
     @Override
-    public List<Tweet> getUserTimeline(@RequestParam("userName") String userName, @RequestParam(value = "pageSize", defaultValue = "20") int pageSize) {
+    public List<Tweet> getUserTimeline(@RequestParam(value = "userName", defaultValue = ZEBRUNNER_NEWS) String userName, @RequestParam(value = "pageSize", defaultValue = "20") int pageSize) {
+        // Verify allowed user names
+        if(!Arrays.asList(ZEBRUNNER_NEWS).contains(userName)) {
+            throw new ForbiddenOperationException("Unsupported user for twitter timeline: " + userName);
+        }
         return twitterService.getUserTimeline(userName, pageSize);
     }
 }
