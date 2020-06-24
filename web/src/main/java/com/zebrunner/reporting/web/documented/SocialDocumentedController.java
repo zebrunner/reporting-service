3178e920195fc9d908ae3c502672b7b336a369b1
Alex Khursevich <hursevich@gmail.com>
2020-06-24 11:55:42 +0300

    Twitter integration adjusted

--- web/src/main/java/com/zebrunner/reporting/web/documented/SocialDocumentedController.java
+++ web/src/main/java/com/zebrunner/reporting/web/documented/SocialDocumentedController.java
@@ -1,12 +1,12 @@
 package com.zebrunner.reporting.web.documented;
 
-import com.zebrunner.reporting.domain.db.Invitation;
-import com.zebrunner.reporting.domain.dto.auth.InvitationListType;
-import com.zebrunner.reporting.domain.dto.auth.InvitationType;
 import com.zebrunner.reporting.domain.dto.errors.ErrorResponse;
-import com.zebrunner.reporting.persistence.dao.mysql.application.search.SearchCriteria;
-import com.zebrunner.reporting.persistence.dao.mysql.application.search.SearchResult;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.social.twitter.api.Tweet;
 
 import java.util.List;
@@ -15,20 +15,19 @@ import java.util.List;
 public interface SocialDocumentedController {
 
     @ApiOperation(
-            value = "Returns user twitter timeline",
-            notes = "Returns user twitter timeline by user name",
-            nickname = "getUserTimeline",
+            value = "Returns Zebrunner tweets",
+            notes = "Returns Zebrunner tweets",
+            nickname = "getUserTweets",
             httpMethod = "GET",
             response = List.class
     )
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userName", paramType = "path", dataType = "String", required = true, value = "User screen name"),
-            @ApiImplicitParam(name = "pageSize", paramType = "path", dataType = "int", required = false, value = "Amount of tweets")
+            @ApiImplicitParam(name = "pageSize", paramType = "query", dataType = "int", value = "Amount of tweets")
     })
     @ApiResponses({
             @ApiResponse(code = 200, message = "Returns user tweets", response = List.class),
-            @ApiResponse(code = 400, message = "Indicates that the userName not specified", response = ErrorResponse.class),
             @ApiResponse(code = 404, message = "Indicates that twitter user not found", response = ErrorResponse.class)
     })
-    List<Tweet> getUserTimeline(String userName, int pageSize);
+    List<Tweet> getZebrunnerTweets(int pageSize);
+
 }
