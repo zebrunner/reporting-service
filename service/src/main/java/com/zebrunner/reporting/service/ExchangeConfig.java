a91b332a594adcb7a50a2c6aec80b0fe2c126d9a
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-07-02 15:13:49 +0300

    Unused import removed

--- service/src/main/java/com/zebrunner/reporting/service/ExchangeConfig.java
+++ service/src/main/java/com/zebrunner/reporting/service/ExchangeConfig.java
@@ -7,7 +7,6 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
