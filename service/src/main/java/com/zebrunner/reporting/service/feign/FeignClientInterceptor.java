c99e5496c88776557040419d974c264204a576c5
tsvrko <tsvrko@gmail.com>
2020-07-02 10:53:31 +0300

    ZBR_HEADER is added to feign calls

--- service/src/main/java/com/zebrunner/reporting/service/feign/FeignClientInterceptor.java
+++ service/src/main/java/com/zebrunner/reporting/service/feign/FeignClientInterceptor.java
@@ -1,6 +1,7 @@
 package com.zebrunner.reporting.service.feign;
 
 import com.zebrunner.reporting.domain.dto.auth.AuthenticatedUser;
+import com.zebrunner.reporting.persistence.utils.TenancyContext;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.security.core.Authentication;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 public class FeignClientInterceptor implements RequestInterceptor {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String ZBR_HEADER = "x-zbr-tenant";
     private static final String TOKEN_TYPE = "Bearer";
 
     @Override
@@ -20,6 +22,7 @@ public class FeignClientInterceptor implements RequestInterceptor {
         if (authentication != null && authentication.getPrincipal() instanceof AuthenticatedUser) {
             AuthenticatedUser authenticatedUser = (AuthenticatedUser) authentication.getPrincipal();
             requestTemplate.header(AUTHORIZATION_HEADER, String.format("%s %s", TOKEN_TYPE, authenticatedUser.getToken()));
+            requestTemplate.header(ZBR_HEADER, TenancyContext.getTenantName());
         }
     }
 
