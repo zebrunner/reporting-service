860a5988c07a4d71bd5fd657b058be438807f8a4
tsvrko <tsvrko@gmail.com>
2020-07-07 12:32:43 +0300

    CreateDefaultUserMessage serialization ia removed

--- domain/src/main/java/com/zebrunner/reporting/domain/push/events/CreateDefaultUserMessage.java
+++ domain/src/main/java/com/zebrunner/reporting/domain/push/events/CreateDefaultUserMessage.java
@@ -1,5 +1,6 @@
 package com.zebrunner.reporting.domain.push.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,18 +8,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class CreateDefaultUserMessage extends EventMessage{
+@AllArgsConstructor
+public class CreateDefaultUserMessage {
 
+    private String tenantName;
     private String tenantUrl;
     private String email;
     private boolean success;
     private String message;
 
-    public CreateDefaultUserMessage(String tenantName, String tenantUrl, String email, boolean success, String message) {
-        super(tenantName);
-        this.tenantUrl = tenantUrl;
-        this.email = email;
-        this.success = success;
-        this.message = message;
-    }
 }
